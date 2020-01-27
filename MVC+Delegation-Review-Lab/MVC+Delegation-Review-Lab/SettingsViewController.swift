2ea1c63c3bdc06c42fbff353f2f17be15ed5f1b7
melinda <melindad_82@msn.com>
2020-01-27 10:08:32 -0500

    initial commmit

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/SettingsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Melinda Diaz on 1/27/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var fontSizeOutlet: UILabel!
+    
+    var allDetailedMovies = Movie.allMovies
+    var fontSize = Float(17.0) {
+    didSet{
+        sliderOutlet.value = fontSize
+        stepperOutlet.value = Double(fontSize)
+        //%0.f means to not have any placements
+        fontSizeOutlet.text = "Font Size \(String(format: "%0.f", fontSize))"
+    }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       daSetUp()
+        
+    }
+    
+    func daSetUp() {
+            stepperOutlet.maximumValue = 50.0
+               sliderOutlet.maximumValue = 50.0
+               stepperOutlet.minimumValue = 5.0
+               sliderOutlet.minimumValue = 5.0
+                
+    }
+    @IBAction func fontSizeChanger(_ sender: UISlider) {
+        fontSize = sliderOutlet.value
+    }
+
+    @IBAction func fontSizeStepper(_ sender: UIStepper) {
+        fontSize = Float(stepperOutlet.value)
+    }
+}
+
--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    
+    var userQuery = "" {
+        didSet {
+            allMovies = Movie.allMovies.filter{$0.name.lowercased().contains(userQuery.lowercased())}
+        }
+
+    }
+    var allMovies = Movie.allMovies {
+        didSet {
+            movieTableView.reloadData()
+        }
+    }
+    // the tableview is able to reload the data when they stop using the search bar
+    var isSearchBarEmpty: Bool {
+        return movieSearchBar.text?.isEmpty ?? true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        movieTableView.dataSource = self
+        movieSearchBar.delegate = self
+        movieTableView.delegate = self
     }
 
 
+    @IBAction func settingsBarButton(_ sender: UIBarButtonItem) {
+       //Melinda Don't forget that this is segueing!!!!!!!
+            guard let settingVC = storyboard?.instantiateViewController(identifier: "SettingsViewController") as? SettingsViewController else {
+                fatalError("Could not downcast to DetailedViewController")}
+        //this makes your Barbutton item segue MODULLY below
+        navigationController?.pushViewController(settingVC, animated: true)
+        }
+       
+    }
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = movieTableView.dequeueReusableCell(withIdentifier: "tableCell") as? TableViewCell else { fatalError("Could not downcast to TableViewCell")}
+        cell.movieLabel.text = allMovies[indexPath.row].name
+        cell.movieYearLabel.text = allMovies[indexPath.row].year.description
+        return cell
+        }
+        
+    }
+    
+    
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            allMovies = Movie.allMovies
+               return
+        }
+    userQuery = searchText
+    }
+    //in order for the searchBar to work you need to use a didSet on BOTH userQuery AND allMovie variables or else it does not work completely
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+        
+        
+    }
 }
 
